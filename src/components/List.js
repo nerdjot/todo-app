6821dfe73d12c1f31a074ee6af17ffb2e3f1b887
nerdjot <njsingh3948@gmail.com>
2021-11-03 21:12:02 +0530

    adding empty list

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import "../styles/List.css";
-import AddComponent from "./AddComponent";
+import React, { useState, useEffect } from 'react';
+import '../styles/List.css';
+import AddComponent from './AddComponent';
 
 const Body = () => {
   return (
@@ -9,6 +9,9 @@ const Body = () => {
       <div className="list-date">Saturday, October 23</div>
       <div className="list-view">
         <AddComponent></AddComponent>
+        <div className="list-empty">
+          
+        </div>
       </div>
     </div>
   );
