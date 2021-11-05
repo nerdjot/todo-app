0fe00fad2c977c63a1f14e1ef98a54521574df4b
nerdjot <njsingh3948@gmail.com>
2021-11-05 14:17:20 +0530

    3 files modified

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/List.css';
 import AddComponent from './AddComponent';
+import { FaCheckCircle, FaRegCheckCircle, FaRegCircle } from 'react-icons/fa';
 
 const Body = () => {
   return (
@@ -9,6 +10,16 @@ const Body = () => {
       <div className="list-date">Saturday, October 23</div>
       <div className="list-view">
         <AddComponent></AddComponent>
+        <div className="list-actual">
+          <div className="internal">
+            <FaRegCircle />
+            <div className="internal-internal">in</div>
+          </div>
+          <div className="internal">internal</div>
+          <div className="internal">internal</div>
+          <div className="internal">internal</div>
+        </div>
+
         <div className="list-empty">
           <div></div>
           <div></div>
@@ -42,8 +53,6 @@ const Body = () => {
           <div></div>
           <div></div>
           <div></div>
-
-          
         </div>
       </div>
     </div>
