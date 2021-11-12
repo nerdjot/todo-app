4ca103f345411c7cf0de20a95192b102bcf4fe7e
nerdjot <njsingh3948@gmail.com>
2021-11-12 20:57:22 +0530

    made changes in  add-comp Input

--- src/components/AddComponent.js
+++ src/components/AddComponent.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import {
-  FaPlus,
-} from 'react-icons/fa';
+import { FaPlus, FaRegCircle } from 'react-icons/fa';
 const AddCompoenent = () => {
+  const [inputText, setInputText] = useState('');
+  const [inputInfoFocus, setInputInFocus] = useState(false);
   return (
     <div className="add-comp">
-      <div className="add-comp-icon"><FaPlus /></div>
-      <input className="add-comp-input"
-      placeholder="Add a task"></input>
+      <div className="add-comp-icon">
+        {inputInfoFocus ? <FaRegCircle color="#6e6e6e" /> : <FaPlus />}
+      </div>
+      <input
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        className="add-comp-input"
+        placeholder="Add a task"
+        onFocus={() => {
+          setInputInFocus(true);
+        }}
+        onBlur={() => {
+          setInputInFocus(false);
+        }}
+      ></input>
+      {inputText === '' ? <></> : <button className="add-comp-btn">ADD</button>}
     </div>
   );
 };
