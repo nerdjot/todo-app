852eca2d41b96706c8b30447c2554a7cc7f9e62d
nerdjot <njsingh3948@gmail.com>
2021-11-16 19:35:52 +0530

    Made filters

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/SideBar.css';
-import { FaSun } from 'react-icons/fa';
-import FilterComp from 'FilterComp.js';
+import { FaSun, FaCalendarAlt, FaStar } from 'react-icons/fa';
+import FilterComp from './FilterComp.js';
 
 const SideBar = () => {
-  return (
-    <div className="side-bar">
-      <div className="filter-comp">
-        <div className="filter-icon">
-          <FaSun />
-        </div>
-        <div className="filter-name">My day</div>
-      </div>
-    </div>
-  );
+  const Filters = [
+    {
+      id: 0,
+      name: 'My day',
+      icon: FaSun,
+    },
+    {
+      id: 1,
+      name: 'Planned',
+      icon: FaCalendarAlt,
+    },
+    {
+      id: 2,
+      name: 'Important',
+      icon: FaStar,
+    },
+  ];
+
+  const FiltersRends = Filters.map((Filter) => {
+    return (
+      <FilterComp
+        key={Filter['id']}
+        name={Filter['name']}
+        Icon={Filter['icon']}
+      />
+    );
+  });
+  return <div className="side-bar">{FiltersRends}</div>;
 };
 
 export default SideBar;
